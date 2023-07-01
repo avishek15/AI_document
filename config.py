8bb428ffcd5f2e3c3394360db5713365752d7b5b
Avishek Majumder <avishekalex15@gmail.com>
2023-07-01 21:59:17 +0800

    Changed context length

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ model_cache = "./model_cache"
 docs_cache = "./docs_cache"
 upload_dir = "./upload_dir"
 collection_name = "docs"
-context_len = 8196
-overlap = 1024
+context_len = 2048
+overlap = 128

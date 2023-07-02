c510bad2204df03943e03e118153985602b4aa9f
Avishek Majumder <avishekalex15@gmail.com>
2023-07-03 00:47:39 +0800

    Add streamlit app

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import shutil
+import os
+
+from chroma_handler import process_pdf
+from chatbot2 import chatbot_response
+from config import upload_dir
+
+
+def sidebar():
+    sidebar = st.sidebar
+    sidebar.title("This is the sidebar")
+    with sidebar.form(key='file-handler', clear_on_submit=True):
+        uploaded_files = st.file_uploader(label="PDF Files",
+                                          type="pdf",
+                                          accept_multiple_files=True,
+                                          key="pdf_upload")
+        submitted = st.form_submit_button("Upload")
+        if uploaded_files and submitted:
+            if os.path.exists(upload_dir):
+                shutil.rmtree(upload_dir)
+            os.makedirs(upload_dir)
+            for fl in uploaded_files:
+                with open(os.sep.join([upload_dir, fl.name]), "wb") as f:
+                    f.write(fl.getbuffer())
+            for f in uploaded_files:
+                process_pdf(os.sep.join([upload_dir, f.name]))
+            st.write("Upload successful!")
+
+
+def main():
+    sidebar()
+    st.title("This is the main content")
+
+    # handle chats till now
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["message"])
+
+    if query := st.chat_input("Ask a question!"):
+        with st.chat_message("user"):
+            st.markdown(query)
+            st.session_state.messages.append({"role": "user",
+                                              "message": query})
+        with st.chat_message("bot"):
+            # bot_response = f"Echo: {query}"
+            bot_response = chatbot_response(query)
+            st.markdown(bot_response)
+            st.session_state.messages.append({"role": "bot",
+                                              "message": bot_response})
+
+
+if __name__ == '__main__':
+    main()

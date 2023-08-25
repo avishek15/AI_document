77f69d75f5842be207711a36de4f5ea29ba9115a
Avishek Majumder <avishekalex15@gmail.com>
2023-08-25 21:04:54 +0800

    Added Book name search

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ model_cache = "./model_cache"
 docs_cache = "./docs_cache"
 upload_dir = "./upload_dir"
 collection_name = "docs"
+book_collection_name = "book_names"
 context_len = 2048
 overlap = 128

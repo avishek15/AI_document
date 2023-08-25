6afac1a5aad5b0e285158547a9ae71107b910a78
Avishek Majumder <avishekalex15@gmail.com>
2023-08-25 21:07:09 +0800

    Implemented Agent
    Agent implementation with memory, but less control.

--- chatbot_as_function.py
+++ chatbot_as_function.py
@@ -1,14 +1,16 @@
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import Tool, AgentExecutor
+from langchain.chains.conversation.memory import ConversationSummaryMemory, ConversationBufferMemory
+from langchain.agents import initialize_agent, AgentType, ZeroShotAgent
 from dotenv import load_dotenv, find_dotenv
-from chroma_handler import process_query
-
+from chroma_handler import process_query, get_top_page
+from langchain.chains.summarize import load_summarize_chain
 
 _ = load_dotenv(find_dotenv())
 
 llm_openai = ChatOpenAI(temperature=0, model_name='gpt-4-0613')
-
 
 template = """
 You are a helpful AI assistant named 'AJ4X' who speaks like \
@@ -32,6 +34,7 @@ ajax_no_memory = PromptTemplate(template=template, input_variables=['question', 
 
 ajax_chain = LLMChain(llm=llm_openai, prompt=ajax_no_memory, verbose=True)
 
+
 # while True:
 def chatbot(query):
     if query == 'q':
@@ -39,8 +42,8 @@ def chatbot(query):
 
     context = process_query(query)
     contexts = [context['documents'][0][i] for i in range(len(context['documents'][0]))]
-    
-    print('COntexts {}'.format(contexts))
+
+    print('Contexts {}'.format(contexts))
     refined_answer = ""
     # answer = ajax_chain.apply([{'question': query,
     #                             'query_context': ctx}
@@ -51,3 +54,64 @@ def chatbot(query):
     # refined_answer = ajax_chain.run(question=query, query_context=next_context)
 
     return f"Hey! It's Aj4X again : {refined_answer}"
+
+
+# def get_summary():
+# docs = ###Get all the documents of that book
+
+# summary_llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+summary_chain = load_summarize_chain(llm_openai, chain_type="stuff")
+
+top_page_tool = Tool(
+    name='Top Page Result',
+    func=get_top_page,
+    description="The database contains pages of the books in questions. "
+                "Use this function to retrieve any particular page of the books stored in the database."
+                "The argument to this function should be - book name: document query"
+)
+
+# summary_tool = Tool(
+#     name='Summarizer',
+#     func=summary_chain.run,
+#     description="Summarize the book mentioned in a paragraph. The reader should be able to grasp what happened in the book."
+# )
+
+all_tools = [top_page_tool]
+# memory = ConversationSummaryMemory(llm=llm_openai, input_key="question")
+
+# simple_agent = initialize_agent(
+#     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#     tools=all_tools,
+#     llm=llm_openai,
+#     verbose=True,
+#     # memory=memory
+# )
+
+prefix = """Have a conversation with a human, \
+            answering the following questions as best you can. 
+            You have access to the following tools:"""
+suffix = """Begin!"
+
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
+
+prompt = ZeroShotAgent.create_prompt(
+    all_tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["input", "chat_history", "agent_scratchpad"],
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm_chain = LLMChain(llm=llm_openai, prompt=prompt)
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=all_tools, verbose=True)
+simple_agent = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=all_tools, verbose=True, memory=memory
+)
+
+
+# print(simple_agent.agent.llm_chain.prompt.template)
+
+
+def conv_agent(query):
+    return simple_agent.run(input=query)

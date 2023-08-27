dc0a1b288155cd22d97ecdf84e8ef5042cf6e4ff
Avishek Majumder <avishekalex15@gmail.com>
2023-08-27 13:00:45 +0800

    Added Spinner

--- src/app.py
+++ src/app.py
@@ -20,13 +20,14 @@ def sidebar():
             # if os.path.exists(upload_dir):
             #     shutil.rmtree(upload_dir)
             # No need to remove path, if path doesn't exists, just create one
-            if not os.path.exists(upload_dir):
-                os.makedirs(upload_dir)
-            for fl in uploaded_files:
-                with open(os.sep.join([upload_dir, fl.name]), "wb") as f:
-                    f.write(fl.getbuffer())
-            for f in uploaded_files:
-                process_pdf(os.sep.join([upload_dir, f.name]))
+            with st.spinner("Please wait for the process to finish..."):
+                if not os.path.exists(upload_dir):
+                    os.makedirs(upload_dir)
+                for fl in uploaded_files:
+                    with open(os.sep.join([upload_dir, fl.name]), "wb") as f:
+                        f.write(fl.getbuffer())
+                for f in uploaded_files:
+                    process_pdf(os.sep.join([upload_dir, f.name]))
             st.write("Upload successful!")
 
 

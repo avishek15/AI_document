b144da1b3ec7a9ad36ac3cc40ded8995d8c8fbb5
Avishek Majumder <avishekalex15@gmail.com>
2023-08-27 13:56:25 +0800

    Add absolute path

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run -p 80:8501 \
-  -v ./conversation_cache:conversation_cache \
-  -v ./docs_cache:docs_cache \
-  -v ./memory_cache:memory_cache \
-  -v ./model_cache:model_cache \
-  -v ./upload_dir:upload_dir \
+  -v ./conversation_cache:/app/conversation_cache \
+  -v ./docs_cache:/app/docs_cache \
+  -v ./memory_cache:/app/memory_cache \
+  -v ./model_cache:/app/model_cache \
+  -v ./upload_dir:/app/upload_dir \
   --rm -it AI_document:v0.1 /bin/bash
